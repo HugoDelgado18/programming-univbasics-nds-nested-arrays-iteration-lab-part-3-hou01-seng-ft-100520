fb6ffadbbddec3b579dfec1639e525f205a54968
Hugo Delgado <hugo.delgado1@outlook.com>
2020-08-22 03:20:57 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -15,7 +15,7 @@ def join_nested_strings(src)
   end
   #mixed_data = mixed_data.join(' ')
   mixed_data = mixed_data.join(' ')
-  mixed_data.delete_at(Integer)
+  mixed_data.delete_at()
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
 end
