01762d118649c114596a50fc53e9e4ef2f4e2264
Hugo Delgado <hugo.delgado1@outlook.com>
2020-08-22 03:33:31 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,14 +7,15 @@ def join_nested_strings(src)
     element_index = 0
 
     while element_index < src[row_index].length do
-    #  if src[row_index][element_index] ==
+      if src[row_index][element_index].class = string
       mixed_data << src[row_index][element_index].to_s
+      end
       element_index += 1
     end
     row_index += 1
   end
   #mixed_data = mixed_data.join(' ')
-  mixed_data.delete_at(4)
+#  mixed_data.delete_at(4)
   mixed_data = mixed_data.join(' ')
 #  mixed_data.delete_at(4, -1, 30, 101, 233)
   # src will be an Array of Arrays of Strings and Integers
