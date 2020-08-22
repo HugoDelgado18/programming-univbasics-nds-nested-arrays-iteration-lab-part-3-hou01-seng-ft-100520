49aecd0f2bd32113c6be82a7cada0474a4fe857e
Hugo Delgado <hugo.delgado1@outlook.com>
2020-08-22 03:36:14 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def join_nested_strings(src)
     element_index = 0
 
     while element_index < src[row_index].length do
-      if src[row_index][element_index].class = string
+      if src[row_index][element_index].object.is_a?(string)
       mixed_data << src[row_index][element_index].to_s
       end
       element_index += 1
